c190a639de5240c25552330989042c5bb6f3b5c3
tom <thomas.mcnutt.jr@gmail.com>
2022-01-04 23:15:54 -0500

    linked list problems

---
+++ src/LinkedListCycle.java
@@ -0,0 +1,45 @@
+public class LinkedListCycle {
+    /*
+     * Leetcode 141
+     * easy
+     * linked list question
+     *
+     */
+    public static void main(String[] args) {
+
+    }
+
+    public static boolean hasCycle(ListNode head) {
+        if (head == null) {
+            return false;
+        }
+
+        ListNode slow = head;
+        ListNode fast = head.next;
+        while (slow != fast) {
+            if (fast == null || fast.next == null) {
+                return false;
+            }
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return true;
+    }
+
+    public static class ListNode {
+        int val;
+        ListNode next;
+
+        public ListNode() {
+        }
+
+        public ListNode(int val) {
+            this.val = val;
+        }
+
+        public ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+}
--- src/MergeTwoSortedLists.java
+++ src/MergeTwoSortedLists.java
@@ -4,23 +4,67 @@ class Main {
     }
 
 
-    public static ListNode mergeTwoSortedLists(ListNode l1, ListNode l2) {
-        ListNode temp = new ListNode();
-        
-        //bruteforce approach would be to add all the nodes to an array, then sort, then add back 
-        //to a linked list
-        //empty lists
-        if (l1 == null && l2 == null) {
-            return null;
+    public static ListNode mergeTwoSortedLists(ListNode list1, ListNode list2) {
+        /*
+        //recursive implementation
+        if (list1 == null) {
+            return list2;
         }
-        return temp;
+
+        if (list2 == null) {
+            return list1;
+        }
+
+        ListNode head;
+
+        if (list1.val < list2.val) {
+            head = list1;
+            list1 = list1.next;
+        } else {
+            head = list2;
+            list2 = list2.next;
+        }
+
+        head.next = mergeTwoLists(list1, list2);
+
+        return head;
+        */
+
+        //iterative implementation
+        ListNode head = new ListNode();
+        ListNode tail = head;
+
+        while (list1 != null && list2 != null) {
+            if (list1.val < list2.val) {
+                tail.next = list1;
+                list1 = list1.next;
+            } else {
+                tail.next = list2;
+                list2 = list2.next;
+            }
+            tail = tail.next;
+        }
+
+        tail.next = list1 == null ? list2 : list1;
+
+        return head.next;
+
     }
 
     public static class ListNode {
-     int val;
-     ListNode next;
-     ListNode() {}
-     ListNode(int val) { this.val = val; }
-     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
     }
 }
